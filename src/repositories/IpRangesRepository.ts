42c2d06abb4762cd33ce50dca9109161beb289e2
Thiago Perin <thiagoperin2310@gmail.com>
2025-06-15 20:19:19 -0300

    feat: add verification if id is NaN

--- src/repositories/IpRangesRepository.ts
+++ src/repositories/IpRangesRepository.ts
@@ -5,7 +5,10 @@ import { ipToID } from "../utils/ip-calculator";
 export class IpRangesRepository {
     async findIpRange(ip: string) {
         const ipRanges = getIpRanges();
+
         const id = ipToID(ip);
+        if (isNaN(id)) return null;
+        
         return findIpRangeById(ipRanges, id);
     }
 }
