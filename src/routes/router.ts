179608363cdeae2c358d6bbb133c49941501002d
Thiago Perin <thiagoperin2310@gmail.com>
2025-06-15 17:12:41 -0300

    refactor: integrate IpRangesController with API route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,15 +1,8 @@
 import { Router } from "express";
+import { IpRangesController } from "../controllers/ipRangesController";
 
 const apiRouter = Router();
 
-apiRouter.get('/', (req, res) => {
-  res.send('Teste rota base da API');
-});
-
-apiRouter.get('/ip/location', (req, res) => {
-  res.statusCode = 404;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ error: 'Status 404: Not Found' }));
-});
+apiRouter.get('/ip/location', (req, res) => { IpRangesController.findIpRange(req, res) });
 
 export default apiRouter;
